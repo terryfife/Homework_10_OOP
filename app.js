0817accea0dc1328f85a20b8a7c5754f6dfc84d6
Terry Fife <tfife@mac.com>
2021-01-27 19:31:26 -0500

    updates to classes

--- app.js
+++ app.js
@@ -10,9 +10,85 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeArray = []
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function addManager() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "What is the name of the manager?"
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "What is the id of the manager?"
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "What is the email of the manager?"
+    },
+    {
+      type: "input",
+      name: "managerOfficeNumber",
+      message: "What is the office number of the manager?"
+    }
+
+  ]).then((answers) => {
+    
+    const newManager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+
+    //console.log(newManager)
+    employeeArray.push(newManager)
+
+    console.log(newManager.getOfficeNumber())
+  })
+}
+
+// create an addEngineer function
+function addEngineer() {
+  
+}
+
+
+// create an addIntern function
+function addIntern() {
+  
+}
+
+// main prompt function
+function addEmployee() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "employeeType",
+      message: "Which employee type would you like to add?",
+      choices: [
+        "Manager",
+        "Engineer",
+        "Intern",
+        "Exit"
+      ]
+    }
+  ]).then((answer) => {
+    
+      if(answer.employeeType === 'Manager') {
+        addManager();
+      } else if(answer.employeeType === 'Engineer')  {
+        addEngineer();
+      } else if (answer.employeeType === 'Intern') {
+        addIntern();
+      } else {
+        // use the render function from above ---> render(employeeArray)
+
+      }
+  })
+}
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +109,4 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
